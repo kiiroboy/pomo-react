619d3f3d9f926b2b4afee3d72951ffb65480f1ae
Tuan <tath2701@gmail.com>
2022-02-24 01:29:20 -0500

    finished core functionality of timer

--- src/Buttons/PlayPauseButton.js
+++ src/Buttons/PlayPauseButton.js
@@ -1,20 +1,10 @@
 import React from "react"
 class PlayPauseButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state={
-            playing:false
-        }
-    }
-
-    handlePlayerClick = () => {
-        this.setState({playing: !this.state.playing})
-    }
     render() {
-        if (this.state.playing) {
+        if (!this.props.status) {
             return (
-                <button onClick={this.handlePlayerClick}>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#bf616a">
+                <button onClick={this.props.onClick}>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
@@ -23,8 +13,8 @@ class PlayPauseButton extends React.Component {
 
         } else {
             return (
-                <button onClick={this.handlePlayerClick}>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#bf616a">
+                <button onClick={this.props.onClick}>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                 </button>
--- src/Timer.js
+++ src/Timer.js
@@ -3,20 +3,94 @@ import 'react-circular-progressbar/dist/styles.css';
 import PlayPauseButton from './Buttons/PlayPauseButton';
 import './custom-timer.css';
 import ResetButton from './Buttons/ResetButton';
+import React from 'react';
+
+const initial = 10;
+class Timer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            time:this.secondsToTime(initial),
+            playing:false,
+            seconds: initial,
+            percent: 100.0,
+        }
+        this.timer = 0;
+        this.startTimer = this.startTimer.bind(this);
+        this.countDown = this.countDown.bind(this);
+    }
+    secondsToTime(secs){
+        let hours = Math.floor(secs / (60 * 60));
+    
+        let divisor_for_minutes = secs % (60 * 60);
+        let minutes = Math.floor(divisor_for_minutes / 60);
+    
+        let divisor_for_seconds = divisor_for_minutes % 60;
+        let seconds = Math.ceil(divisor_for_seconds);
+    
+        let obj = {
+          "h": hours,
+          "m": minutes,
+          "s": seconds
+        };
+        return obj;
+    } 
+
+    startTimer() {
+        if (this.timer === 0 && this.state.seconds > 0) {
+            this.timer = setInterval(this.countDown, 1000);
+        }
+    }
+
+    countDown() {
+        // Remove one second, set state so a re-render happens.
+        let sec = this.state.seconds - 1;
+        this.setState({
+            time: this.secondsToTime(sec),
+            seconds: sec,
+            percent: this.state.percent - (100/initial)
+        });
+        
+        // Check if we're at zero.
+        if (sec === 0) { 
+            clearInterval(this.timer);
+            this.timer = 0;
+        }
+    }
 
 
-const percent = 70;
-var time = 90;
-function Timer() {
-    return (
-        <div class="core">
-            <CircularProgressbar value={percent} text={`${Math.floor(time/60)}:${(time%60).toString().padStart(2,'0')}`} strokeWidth={3}/>
-            <div>
-               <PlayPauseButton/>
-               <ResetButton/>
+    handlePlayPause = () => {
+        if (!this.state.playing) {
+            this.startTimer();
+        } else {
+            clearInterval(this.timer);
+            this.timer = 0;
+        }
+        this.setState({
+            playing: !this.state.playing,
+        });
+    }
+    handleReset = () => {
+        this.setState({
+            time:this.secondsToTime(initial),
+            playing:false,
+            seconds: initial,
+            percent: 100.0,
+        });
+        clearInterval(this.timer);
+        this.timer = 0;
+    }
+    render() {
+        return (
+            <div class="core">
+                <CircularProgressbar value={this.state.percent} text={`${this.state.time.m}:${this.state.time.s.toString().padStart(2,'0')}`} strokeWidth={3}/>
+                <div>
+                <PlayPauseButton status={this.state.playing} onClick={this.handlePlayPause}/>
+                <ResetButton onClick={this.handleReset}/>
+                </div>
+                
             </div>
-            
-        </div>
-    )
+        );
+    }
 }
 export default Timer;
