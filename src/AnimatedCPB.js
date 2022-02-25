59699dc17297d7eff5021af03ee7f620e4a05025
Tuan <tath2701@gmail.com>
2022-02-24 19:38:22 -0500

    update smoothing

--- src/AnimatedCPB.js
+++ src/AnimatedCPB.js
@@ -2,45 +2,76 @@ import React from "react";
 import { Animate } from "react-move";
 
 class AnimatedCPB extends React.Component {
-  interval = undefined;
-
-  state = {
-    isAnimated: this.props.playing
-  };
+  constructor(props) {
+      super(props);
+      this.state = {
+        isAnimated: false,
+      }
+  }
+  componentDidMount() {
+      this.setState({
+        isAnimated: !this.state.isAnimated
+      });
+  }
+  componentDidUpdate() {
+    console.log("update");
+    if(this.state.isAnimated === false) {
+        this.state.isAnimated = true;
+    }
+    this.render();
+  }
 
   static defaultProps = {
     valueStart: 0
   };
 
-  componentDidMount() {
-      if (!this.state.isAnimated) {
-        this.setState({isAnimated: true})
-      } else {
-        this.setState({isAnimated: false})
-      }
-  }
-
-  componentWillUnmount() {
-    window.clearInterval(this.interval);
-  }
-
   render() {
+    if (this.props.reset === true) {
+      this.state.isAnimated = false; 
+      return (
+        <Animate
+          start={() => ({
+            value: this.props.valueStart,
+            seconds: this.props.duration
+          })}
+          update={() => ({
+            value: [
+              this.props.valueStart
+            ],
+            seconds: [
+              this.props.duration 
+            ],
+            timing: {
+              duration: 0
+            }
+          })}
+        >
+          {({value, seconds}) => this.props.children(value, seconds)}
+        </Animate>
+      );
+
+    }
     return (
       <Animate
         start={() => ({
-          value: this.props.valueStart
-        })}r
-        update={() => ({
+          value: this.props.valueStart,
+          seconds: this.props.duration,
+        })}
+        update={() => {
+          return {
           value: [
             this.state.isAnimated ? this.props.valueEnd : this.props.valueStart
           ],
+          seconds: [
+            this.state.isAnimated ? 0: this.props.duration 
+          ],
           timing: {
             duration: this.props.duration * 1000,
-            ease: this.props.easingFunction
           }
-        })}
+        }
+        }}
       >
-        {({ value }) => this.props.children(value)}
+        {({value, seconds}) => this.props.children(value, seconds)}
       </Animate>
     );
   }
--- src/Timer.js
+++ src/Timer.js
@@ -5,20 +5,17 @@ import './custom-timer.css';
 import ResetButton from './Buttons/ResetButton';
 import React from 'react';
 import AnimatedCPB from './AnimatedCPB';
-import {easeQuadInOut} from "d3-ease"
-const initial = 10;
+const initial = 5;
 class Timer extends React.Component {
     constructor(props) {
         super(props);
         this.state={
-            time:this.secondsToTime(initial),
             playing:false,
-            seconds: initial,
-            percent: 100.0,
         }
-        this.timer = 0;
-        this.startTimer = this.startTimer.bind(this);
-        this.countDown = this.countDown.bind(this);
+        this.reset=false;
+        this.currVal=0;
+        this.currDur=initial;
+        this.interval = undefined;
     }
     secondsToTime(secs){
         let hours = Math.floor(secs / (60 * 60));
@@ -37,76 +34,73 @@ class Timer extends React.Component {
         return obj;
     } 
 
-    startTimer() {
-        if (this.timer === 0 && this.state.seconds > 0) {
-            this.timer = setInterval(this.countDown, 1000);
-        }
-    }
-
-    countDown() {
-        // Remove one second, set state so a re-render happens.
-        let sec = this.state.seconds - 1;
-        this.setState({
-            time: this.secondsToTime(sec),
-            seconds: sec,
-            percent: this.state.percent - (100/initial)
-        });
-        
-        // Check if we're at zero.
-        if (sec === 0) { 
-            clearInterval(this.timer);
-            this.timer = 0;
-        }
-    }
-
-
     handlePlayPause = () => {
+        console.log(this.state.playing?"pause":"play");
         if (!this.state.playing) {
             this.setState({
                 playing: !this.state.playing,
             });
-            this.startTimer();
+            this.interval = setTimeout(() => {
+                this.handleReset();
+                window.clearInterval(this.interval);
+            }, this.currDur*1000);
         } else {
-            clearInterval(this.timer);
-            this.timer = 0;
             this.setState({
                 playing: !this.state.playing,
             });
         }
     }
     handleReset = () => {
+        this.currDur = initial;
+        this.currVal = 0;
+        this.reset = true;
+        console.log("1");
         this.setState({
-            time:this.secondsToTime(initial),
-            playing:false,
-            seconds: initial,
-            percent: 100.0,
-        });
-        clearInterval(this.timer);
-        this.timer = 0;
+            playing: false,
+        })
     }
     render() {
-        return (
-            <div class="core">
-                <AnimatedCPB
-                    valueStart={45}
-                    valueEnd={100}
-                    duration={initial}
-                    easingFunction={easeQuadInOut}
-                    playing={this.state.playing}
-                    >
-                    {value => {
-                        return (
-                            <CircularProgressbar value={value} text={`${this.state.time.m}:${this.state.time.s.toString().padStart(2,'0')}`} strokeWidth={3} styles={buildStyles({pathTransition:"none"})}/>
-                        );
-                    }}
-                </AnimatedCPB>
-                <div>
-                <PlayPauseButton status={this.state.playing} onClick={this.handlePlayPause}/>
-                <ResetButton onClick={this.handleReset}/>
+        if (this.state.playing) {
+            return (
+                <div class="core">
+                    <AnimatedCPB
+                        valueStart={this.currVal}
+                        valueEnd={100}
+                        duration={this.currDur}
+                        playing={this.state.playing}
+                        reset={this.reset}
+                        >
+                        {(value, seconds) => {
+                            const sec = this.secondsToTime(Math.round(seconds));
+                            this.currVal = value;
+                            this.currDur = Math.round(seconds);
+                            this.playing=false;
+                            this.reset = false;
+                            return (
+                                <CircularProgressbar value={value} text={`${sec.m.toString()}:${sec.s.toString().padStart(2,'0')}`} strokeWidth={3} styles={buildStyles({pathTransition:value===0? "none":"none"})}/>
+                            );
+                        }}
+                    </AnimatedCPB>
+                    <div>
+                    <PlayPauseButton status={this.state.playing} onClick={this.handlePlayPause}/>
+                    <ResetButton onClick={this.handleReset}/>
+                    </div>
                 </div>
-                
-            </div>
-        );
+            );
+        } else {
+            let sec = this.secondsToTime(Math.round(this.currDur));
+            console.log(sec);
+            return (
+                <div class="core">
+                    <CircularProgressbar value={this.currVal} text={`${sec.m.toString()}:${sec.s.toString().padStart(2,'0')}`} strokeWidth={3} styles={buildStyles({pathTransition:this.currDur===0? "none":"none"})}/>
+                    <div>
+                    <PlayPauseButton status={this.state.playing} onClick={this.handlePlayPause}/>
+                    <ResetButton onClick={this.handleReset}/>
+                    </div>
+                </div>
+            );
+            
+        }
     }
 }
 export default Timer;
