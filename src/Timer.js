a97ac589f4e1850f30068ca60767177d294079a2
Tuan <tath2701@gmail.com>
2022-02-28 12:56:03 -0500

    update cirularprogressbar

--- src/Timer.js
+++ src/Timer.js
@@ -7,7 +7,8 @@ import React from 'react';
 import {Animate} from 'react-move';
 import SettingButton from './Buttons/SettingButton';
 
-const initialT = 0
+const initialT =25
+const speed = 100
 class Timer extends React.Component {
     constructor(props) {
         super(props);
@@ -23,18 +24,11 @@ class Timer extends React.Component {
         this.timeout = undefined;
     }
     componentDidMount() {
-        this.resetDisplay(initialT);
+        console.log("did mount");
+        this.updateDisplay(initialT);
     }
-    updateDisplay(type, value) {
-        if(isNaN(value)) {
-            value = '0';
-        }
-        let str = value.toString();
-        if(value.toString().length < 2) {
-            str = str.padStart(2,'0');
-        }else {
-            str = str.slice(-2);
-        }
+    updateTime(type, value) {
+        let str = this.autopad(value);
         let res = isNaN(parseInt(str)) ? 0: parseInt(str);
         if (res > 60) {
             res = 59;
@@ -61,13 +55,19 @@ class Timer extends React.Component {
 
         
     }
-    resetDisplay(value) {
-        console.log("when pause", value);
+    autopad(str){
+        str =  str.toString();
+        if(str.length < 2) {
+            return str.padStart(2,'0');
+        }
+        return str.slice(-2);
+    }
+    updateDisplay(value) {
         let sec = this.secondsToTime(value);
-        console.log(sec.h, sec.m, sec.s);
-        this.setState({hours:sec.h});
-        this.setState({minutes:sec.m});
-        this.setState({seconds:sec.s});
+        console.log(this.autopad(sec.h));
+        this.setState({hours:this.autopad(sec.h)});
+        this.setState({minutes:this.autopad(sec.m)});
+        this.setState({seconds:this.autopad(sec.s)});
     }
     secondsToTime(secs){
         let hours = Math.floor(secs / (60 * 60));
@@ -88,35 +88,33 @@ class Timer extends React.Component {
 
     handlePlayPause = () => {
         if(!this.state.playing) {
+            
             this.setState({
                 playing: !this.state.playing
             })
             this.timeout = setTimeout(()=> {
                 this.handleReset()
-            },this.currDur*1000)
+            },this.currDur*speed)
         } else {
-            window.clearTimeout(this.timeout);
             this.setState({
                 playing: !this.state.playing,
             });
-            this.resetDisplay(Math.round(this.currDur));
+            this.updateDisplay(Math.round(this.currDur));
+            window.clearTimeout(this.timeout);
         }
     }
 
     handleReset = () => {
-        console.log("state when press reset", this.state.initial, this.currVal);
-        this.currVal = 0;
-        this.currDur = this.state.initial
-        console.log("state when press reset", this.state.initial, this.currVal);
-        window.clearTimeout(this.timeout);
-        this.resetDisplay(Math.round(this.state.initial));
         this.setState({
             playing: false,
         });
+        this.currVal = 0;
+        this.currDur = this.state.initial
+        window.clearTimeout(this.timeout);
+        this.updateDisplay(Math.round(this.state.initial));
     }
 
     updateInput(type,value) {
-        console.log("value", value)
         let s = (this.currDur%60);
         let m = Math.floor(this.currDur/60)%60;
         let h = Math.floor(this.currDur/3600)%60;
@@ -129,88 +127,9 @@ class Timer extends React.Component {
         }
         this.setState({initial: this.currDur});
         this.currDur = this.state.initial;
-        console.log("this is dur",this.currDur);
-        console.log("this is initial",this.state.initial);
         
     }
-
-    handleOnBlurS = (e) => {
-        let res = parseInt(e.target.value);
-        if (res > 60) {
-            res = 59;
-        } else if (res === 60) {
-            res = 0;
-        } else {
-            res = isNaN(parseInt(e.target.value)) ? 0: parseInt(e.target.value);
-        }
-
-        this.updateInput("s", res);
-    }
-
-    handleKeyDownS = (e) => {
-        if (e.key === 'Enter') {
-            let res = parseInt(e.target.value);
-            if (res > 60) {
-                res = 59;
-            } else if (res === 60) {
-                res = 0;
-            } else {
-                res = isNaN(parseInt(e.target.value)) ? 0: parseInt(e.target.value);
-            }
-            this.updateInput("s", res);
-        }
-    }
-    handleOnBlurM = (e) => {
-        let res = parseInt(e.target.value);
-        if (res > 60) {
-            res = 59;
-        } else if (res === 60) {
-            res = 0;
-        } else {
-            res = isNaN(parseInt(e.target.value)) ? 0: parseInt(e.target.value);
-        }
-
-        this.updateInput("m", res);
-    }
-
-    handleKeyDownM = (e) => {
-        if (e.key === 'Enter') {
-            let res = parseInt(e.target.value);
-            if (res > 60) {
-                res = 59;
-            } else if (res === 60) {
-                res = 0;
-            } else {
-                res = isNaN(parseInt(e.target.value)) ? 0: parseInt(e.target.value);
-            }
-            this.updateInput("m", res);
-        }
-    }
-    handleKeyDownH = (e) => {
-        if (e.key === 'Enter') {
-            let res = parseInt(e.target.value);
-            if (res > 60) {
-                res = 59;
-            } else if (res === 60) {
-                res = 0;
-            } else {
-                res = isNaN(parseInt(e.target.value)) ? 0: parseInt(e.target.value);
-            }
-            this.updateInput("h", res);
-        }
-    }
-    handleOnBlurH = (e) => {
-        let res = parseInt(e.target.value);
-        if (res > 60) {
-            res = 59;
-        } else if (res === 60) {
-            res = 0;
-        } else {
-            res = isNaN(parseInt(e.target.value)) ? 0: parseInt(e.target.value);
-        }
-
-        this.updateInput("h", res);
-    }
+    
     render() {
         return (
             <div class>
@@ -227,7 +146,7 @@ class Timer extends React.Component {
                         this.state.playing ? 0: this.currDur
                     ],
                     timing: {
-                        duration: [this.state.playing ? this.currDur*1000: 0]
+                        duration: [this.state.playing ? this.currDur*speed: 0]
                     }
                 })}
                 >
@@ -240,15 +159,19 @@ class Timer extends React.Component {
                                 <CircularProgressbar value={value} strokeWidth={3} styles={buildStyles({pathTransition:"none"})}/>
                                 <div class="child">
                                 <label class="text">
-                                    <input class="activeInput" placeholder={this.state.hours} value={this.state.hours}  onKeyDown={e=>{this.updateDisplay("h",e.target.value)}} onBlur={e=>{this.updateDisplay("h",e.target.value)}} onChange={e=>{this.updateDisplay("h",e.target.value)}}/>
-
+                                    <input class="activeInput" placeholder={this.state.hours} value={this.state.hours}  onKeyDown={e=>{this.updateTime("h",e.target.value)}} onBlur={e=>{this.updateTime("h",e.target.value)}} onChange={e=>{this.updateTime("h",e.target.value)}}/>
                                     :
-                                    <input class="activeInput" placeholder={this.state.minutes} value={this.state.minutes} onKeyDown={e=>{this.updateDisplay("m",e.target.value)}} onBlur={e=>this.updateDisplay("m",e.target.value)} onChange={e=>{this.updateDisplay("m",e.target.value)}}/>
+                                    <input class="activeInput" placeholder={this.state.minutes} value={this.state.minutes} onKeyDown={e=>{this.updateTime("m",e.target.value)}} onBlur={e=>this.updateTime("m",e.target.value)} onChange={e=>{this.updateTime("m",e.target.value)}}/>
                                     :
-                                    <input class="activeInput" placeholder={this.state.seconds} value={this.state.seconds} onKeyDown={e=>{this.updateDisplay("s",e.target.value)}} onBlur={e=>{this.updateDisplay("s",e.target.value)}} onChange={e=>{this.updateDisplay("s",e.target.value)}}/>
+                                    <input class="activeInput" placeholder={this.state.seconds} value={this.state.seconds} onKeyDown={e=>{this.updateTime("s",e.target.value)}} onBlur={e=>{this.updateTime("s",e.target.value)}} onChange={e=>{this.updateTime("s",e.target.value)}}/>
                                 </label>
                                 </div>
                             </div> 
+                            <div class="timer_button">
+                            <PlayPauseButton status={this.state.playing} onClick={this.handlePlayPause}/>
+                            <ResetButton onClick={this.handleReset}/>
+                            <SettingButton></SettingButton>
+                            </div>
                             </div>
                         );
                     } else {
@@ -268,6 +191,11 @@ class Timer extends React.Component {
                                 </label>
                                 </div>
                             </div> 
+                            <div class="timer_button">
+                            <PlayPauseButton status={this.state.playing} onClick={this.handlePlayPause}/>
+                            <ResetButton onClick={this.handleReset}/>
+                            <SettingButton></SettingButton>
+                            </div>
                             </div>
                         );
 
@@ -275,16 +203,6 @@ class Timer extends React.Component {
 
                 }}
                 </Animate>
-                <div class="timer_button">
-                <PlayPauseButton status={this.state.playing} onClick={this.handlePlayPause}/>
-                <ResetButton onClick={this.handleReset}/>
-                <SettingButton></SettingButton>
-                </div>
-                <div class="timer_button">
-                <PlayPauseButton status={this.state.playing} onClick={this.handlePlayPause}/>
-                <ResetButton onClick={this.handleReset}/>
-                <SettingButton></SettingButton>
-                </div>
             </div>
         );
     }
