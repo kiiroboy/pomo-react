58ce8ec8f2725094c1d1084a45c4484a91851173
Tuan <tath2701@gmail.com>
2022-02-28 22:27:33 -0500

    Updates

---
+++ precache-manifest.bd281d029f76804f4e4fa8efb24305ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8253c27eaa0e5935c8da",
+    "url": "/pomo-react/static/css/main.81f70697.chunk.css"
+  },
+  {
+    "revision": "8253c27eaa0e5935c8da",
+    "url": "/pomo-react/static/js/main.ff5ad8c5.chunk.js"
+  },
+  {
+    "revision": "bf492ed92681016c4038",
+    "url": "/pomo-react/static/js/runtime~main.2e718c4b.js"
+  },
+  {
+    "revision": "bc43dcf38bd0cef3967c",
+    "url": "/pomo-react/static/css/2.551a4ea1.chunk.css"
+  },
+  {
+    "revision": "bc43dcf38bd0cef3967c",
+    "url": "/pomo-react/static/js/2.9292caf7.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/pomo-react/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "1da8e478295820f02287a00cbdae55d3",
+    "url": "/pomo-react/static/media/Meteor.1da8e478.svg"
+  },
+  {
+    "revision": "504d2edbf172e51eb08e9cd8ff22fa1e",
+    "url": "/pomo-react/index.html"
+  }
+];
